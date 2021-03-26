69bbbdf55db68b3db7f712edec4854a7d688ee78
Thomas Vanelstlande <thomas_vanelstlande@hotmail.com>
2021-03-26 16:43:17 +0100

    integration of %f wip

--- ft_format_f.c
+++ ft_format_f.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_put_double.c                                    :+:      :+:    :+:   */
+/*   ft_format_f.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tvanelst <tvanelst@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 08:44:19 by tvanelst          #+#    #+#             */
-/*   Updated: 2021/03/25 15:29:06 by tvanelst         ###   ########.fr       */
+/*   Updated: 2021/03/26 16:32:55 by tvanelst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	get_n_size(double n, int data[3], char *flags)
+static int	get_n_size(char *s, int data[3], char *flags)
 {
 	int	size;
-	int n2;
 
-	n2 = n;
-	size = 1;
-	while (n2 / 10)
-	{
-		n2 /= 10;
-		size++;
-	}
+	size = ft_strlen(s);
 	if (data[1] == -1)
 		size += 7;
 	else if (data[1])
@@ -34,13 +27,27 @@ static int	get_n_size(double n, int data[3], char *flags)
 char		*ft_format_f(double n, int data[3], char *flags)
 {
 	int		size;
+	int		size2;
 	char	*s;
+	char	*s2;
 
-	size = get_n_size(n, data, flags);
+	s2 = ft_format_di(n, data, flags);
+	size = get_n_size(s2, data, flags);
 	s = malloc(size + 1);
 	if (!s)
 		return (0);
 	*(s + size--) = 0;
+	size2 = ft_strlcpy(s, s2, size);
+	free(s2);
+	*(s + size2++) = '.';
+	n = n - (int)n;
+	while (size2 < size--)
+		n *= 10;
+	if ((int)n % 5)
+		n = (n + 10) / 10;
+	s2 = ft_itoa(n);
+	return (s2);
+	ft_strlcpy(ft_strchr(s, '.') + 1, s2, 10);
 	if (ft_strchr(flags, '#'))
 	{
 		return (s);
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tvanelst <tvanelst@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 11:01:05 by thomasvanel       #+#    #+#             */
-/*   Updated: 2021/03/25 12:53:23 by tvanelst         ###   ########.fr       */
+/*   Updated: 2021/03/26 15:19:00 by tvanelst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ int		ft_printf(const char *fmt, ...);
 char	*ft_format_s(char *s, int size);
 char	*ft_format_c(char c);
 char	*ft_format_di(int n, int *data, char *flags);
+char	*ft_format_f(double n, int data[3], char *flags);
 #endif
