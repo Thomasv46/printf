40fd703243b0e74f415fe5748c60ae2c0f6c3ee5
Thomas Vanelstlande <thomas_vanelstlande@hotmail.com>
2021-04-10 23:29:12 +0200

    Implementing %g WIP

--- ft_format_g.c
+++ ft_format_g.c
@@ -6,7 +6,7 @@
 /*   By: tvanelst <tvanelst@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 18:20:39 by tvanelst          #+#    #+#             */
-/*   Updated: 2021/04/10 19:46:17 by tvanelst         ###   ########.fr       */
+/*   Updated: 2021/04/10 22:49:39 by tvanelst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,28 @@ static char	*handle_edge_cases(double n, t_converter *c)
 		return (ft_strdup("-inf"));
 	else
 		return (ft_strdup("nan"));
+}
+
+static char	*trim_trailing_0(char *s, t_converter *c)
+{
+	char	*s2;
+	int		size;
+
+	if (!ft_strchr(c->flags, '#'))
+	{
+		s2 = ft_strtrim(s, "0.");
+		free(s);
+		s = s2;
+	}
+	else
+	{
+		size = ft_strlen(s) - 1 ;
+		while (*(s + size) == '0')
+			size--;
+		if (*(s + size) == '.')
+			*(s + size + 1) = 0;
+	}
+	return (s);
 }
 
 char	*ft_format_g(va_list ap, t_converter *c)
@@ -51,7 +73,16 @@ char	*ft_format_g(va_list ap, t_converter *c)
 	if (exponant < -4 || exponant > c->precision)
 		s = ft_format_e(ap2, c);
 	else
+	{
+		c->convertion = 'f';
 		s = ft_format_f(ap2, c);
+	}
+	s = trim_trailing_0(s, c);
 	va_end(ap2);
 	return (s);
 }
+/* #include <stdio.h>
+int main()
+{
+	ft_printf(" %#-03.1g ", 1.0);
+} */
