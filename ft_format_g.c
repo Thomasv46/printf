eb67111efd4041bfea677fc1a56f9b88312ab549
Thomas Vanelstlande <thomas_vanelstlande@hotmail.com>
2021-04-12 18:08:57 +0200

    norminette %g ok

--- ft_format_g.c
+++ ft_format_g.c
@@ -6,22 +6,11 @@
 /*   By: tvanelst <tvanelst@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 18:20:39 by tvanelst          #+#    #+#             */
-/*   Updated: 2021/04/12 17:29:12 by tvanelst         ###   ########.fr       */
+/*   Updated: 2021/04/12 18:07:24 by tvanelst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static char	*handle_edge_cases(double n, t_converter *c)
-{
-	c->pad = ' ';
-	if (n == 1.0 / 0)
-		return (ft_strdup("inf"));
-	else if (n == -1.0 / 0)
-		return (ft_strdup("-inf"));
-	else
-		return (ft_strdup("nan"));
-}
 
 static void	get_number_and_exponent(double n, int *exponent)
 {
@@ -41,31 +30,6 @@ static void	get_number_and_exponent(double n, int *exponent)
 	}
 }
 
-static int	get_size(double n, t_converter *c)
-{
-	int number_of_decimals;
-	int	max;
-
-	max = c->precision;
-	if (c->precision == -1)
-		c->precision = 6;
-	else if (!c->precision)
-		c->precision = 1;
-	if (!(-1 < n && n < 1))
-		number_of_decimals = c->precision;
-	else
-	{
-		number_of_decimals = 0;
-		while ((long long)n == 0 && n)
-		{
-			number_of_decimals++;
-			n *= 10;
-		}
-		number_of_decimals += c->precision - 1;
-	}
-	return (number_of_decimals);
-}
-
 static int	get_int_length(double n)
 {
 	int	i;
@@ -81,10 +45,36 @@ static int	get_int_length(double n)
 	return (i);
 }
 
+static char	*get_size(double n, va_list ap, t_converter *c, char convertion)
+{
+	int		number_of_decimals;
+	int		int_length;
+
+	int_length = get_int_length(n);
+	if (!(-1 < n && n < 1))
+		number_of_decimals = c->precision;
+	else
+	{
+		number_of_decimals = 0;
+		while ((long long)n == 0 && n)
+		{
+			number_of_decimals++;
+			n *= 10;
+		}
+		number_of_decimals += c->precision - 1;
+	}
+	if (convertion == 'e')
+	{
+		c->precision = number_of_decimals - 1;
+		return (ft_format_e(ap, c));
+	}
+	c->precision = number_of_decimals - int_length;
+	return (ft_format_f(ap, c));
+}
+
 char	*ft_format_g(va_list ap, t_converter *c)
 {
 	int		exponent;
-	int		precision;
 	double	n;
 	va_list	ap2;
 	char	*s;
@@ -92,26 +82,19 @@ char	*ft_format_g(va_list ap, t_converter *c)
 	va_copy(ap2, ap);
 	n = va_arg(ap, double);
 	if (n != n || n == 1.0 / 0.0 || n == -1.0 / 0.0)
-		return (handle_edge_cases(n, c));
-	get_number_and_exponent(n, &exponent);
-	if (c->precision == 0)
-		precision = 1;
-	else if (c->precision == -1)
-		precision = 6;
-	else
-		precision = c->precision;
-	c->precision = get_size(n, c);
-	if (exponent < -4 || exponent >= precision)
-	{
-		c->precision -= 1;
-		s = ft_format_e(ap2, c);
-	}
-	else
-	{
-		c->precision -= get_int_length(n);
 		s = ft_format_f(ap2, c);
+	else
+	{
+		get_number_and_exponent(n, &exponent);
+		if (c->precision == 0)
+			c->precision = 1;
+		else if (c->precision == -1)
+			c->precision = 6;
+		if (exponent < -4 || exponent >= c->precision)
+			s = get_size(n, ap2, c, 'e');
+		else
+			s = get_size(n, ap2, c, 'f');
 	}
-
 	va_end(ap2);
 	return (s);
 }
